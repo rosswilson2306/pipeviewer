fa4e4cf9850daa91ddfe29ae0097251e7e985e64
Ross Wilson <ross.wilson@homeflow.co.uk>
2024-07-24 19:03:52 +0100

    fix typo of handle

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,15 @@ fn main() -> Result<()> {
     let (stats_tx, stats_rx) = unbounded();
     let (write_tx, write_rx) = bounded(1024);
 
-    let read_hanlde = thread::spawn(move || read::read_loop(&infile, stats_tx, write_tx));
-    let stats_hanlde = thread::spawn(move || stats::stats_loop(silent, stats_rx));
-    let write_hanlde = thread::spawn(move || write::write_loop(&outfile, write_rx));
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx, write_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
 
     // crash if any threads have crashed
     // `.join()` returns a `thread::Result<io::Result<()>>`
-    let read_io_result = read_hanlde.join().unwrap();
-    let stats_io_result = stats_hanlde.join().unwrap();
-    let write_io_result = write_hanlde.join().unwrap();
+    let read_io_result = read_handle.join().unwrap();
+    let stats_io_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
 
     // Return an error if any threads returned an error
     read_io_result?;
